aa3e1f224009e098b7c14d549e84fb5b9a51dcce
Anh Pham <anh.pham@neighbourfood.fi>
2017-03-15 10:47:17 +0200

    feat: data validation in register endpoint.

--- api/src/main/java/com/kuuasema/RegisterController.java
+++ api/src/main/java/com/kuuasema/RegisterController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 
@@ -48,6 +47,10 @@ class RegisterController {
 
     @RequestMapping(method = RequestMethod.POST)
     ResponseEntity<?> register(@RequestBody RegisterParams params) {
+        if (params.getPlayerName().equals("")) {
+            return Application.InvalidRequest.build();
+        }
+
         Player player = this.playerRepository.findByName(params.getPlayerName());
 
         if (player != null) {
